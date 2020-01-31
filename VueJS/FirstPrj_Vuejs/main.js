f0b611cfe84a964b1b4466d1b75aa06a3c654c17
thang8nq <thang8nq@gmail.com>
2020-01-31 21:25:01 +0700

    *[VueJS] *Remove unnecessary css inside html tag *Init code demo for event modifier (.stopPropagation) (There are 2 box, box2 inside box -> click on box2 mean click on both)

--- VueJS/FirstPrj_Vuejs/main.js
+++ VueJS/FirstPrj_Vuejs/main.js
@@ -3,12 +3,18 @@ var vueInstance = new Vue({
     data: {
         count: 1, 
         posX: 0, 
-        posY: 0
+        posY: 0,
+        posXBox2: 0, 
+        posYBox2: 0
     }, 
     methods: {
         getLocation(e, number){
-            this.posX += number;
+            this.posX = e.clientX;
             this.posY = e.clientY;
+        }, 
+        getLocationChild(e, number){
+            this.posXBox2 = e.clientX; 
+            this.posYBox2 = e.clientY;
         }
     }
 });
