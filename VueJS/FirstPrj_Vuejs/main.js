0886f3c18d2ab47f6637bb23329c67d3315c2d71
thang8nq <thang8nq@gmail.com>
2020-02-05 13:16:09 +0700

    [VueJs] Different when call function/computed

--- VueJS/FirstPrj_Vuejs/main.js
+++ VueJS/FirstPrj_Vuejs/main.js
@@ -1,16 +1,17 @@
 var vueInstance = new Vue({
     el: '#app',
     data: {
-        count: 1, 
-        posX: 0, 
-        posY: 0,
-        posXBox2: 0, 
-        posYBox2: 0
     }, 
     methods: {
-        handleSubmitForm(e){
-            console.log(e);
-           //event.preventDefault();
+        callFunction ()
+        {
+            console.log("We calling a function");
+        }
+    }, 
+    computed: {
+        callComputed()
+        {
+            console.log("We calling a computed");
         }
     }
 });
