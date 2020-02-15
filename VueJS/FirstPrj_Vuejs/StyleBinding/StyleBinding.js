580d2d411a26aef45120046d87e9a88458201ee2
thang8nq <thang8nq@gmail.com>
2020-02-16 06:52:31 +0700

    [VueJs] Style Binding, 1st way to set image background

--- VueJS/FirstPrj_Vuejs/StyleBinding/StyleBinding.js
+++ VueJS/FirstPrj_Vuejs/StyleBinding/StyleBinding.js
@@ -2,7 +2,8 @@ var vueInstance = new Vue({
     el: '.app', 
     data: {
         activeColor: 'blue', 
-        fontSize: 20
+        fontSize: 20, 
+        backgroundImage: 'https://www.thepaintedturtle.org/sites/main/files/main-images/camera_lense_0.jpeg'
     }, 
     methods: {
         
