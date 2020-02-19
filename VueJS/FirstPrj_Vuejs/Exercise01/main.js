49d221567b585f310d6bb3256d1144e573b9419b
thang8nq <thang8nq@gmail.com>
2020-02-19 21:40:14 +0700

    [VueJs] First exercise, update product image display, product color when select.

--- VueJS/FirstPrj_Vuejs/Exercise01/main.js
+++ VueJS/FirstPrj_Vuejs/Exercise01/main.js
@@ -2,9 +2,21 @@ var vueInstance = new Vue({
     el: '#app',
     data: {
         numProductInCart: 0,
+        productDisplay: "red.jpg",
+        productColor: "Màu đỏ",
+        imagePath: "images/",
+        listProduct: [
+            {url: "black.jpg", color: "Màu đen"},
+            {url: "blue.jpg",  color: "Màu xanh"},
+            {url: "red.jpg",   color: "Màu đỏ"}
+        ]
     },
     methods: {
-       
+       updateProductDisplay(item) {
+            this.productDisplay = item.url;
+            this.productColor = item.color;
+       }
+
     },
     computed: {
        
