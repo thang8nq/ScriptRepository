3c079db1d7b9d397db31c31b9253a3e818a76d42
thang8nq <thang8nq@gmail.com>
2020-02-21 11:04:34 +0700

    [VueJs] Example about loop & condition (for and if) - if has priority higher than for. therefore, when use both for and if in this case, it will generate some unexpected html element (check image RenderThua.PNG) - solution is using computed, will available in next commit

---
+++ VueJS/FirstPrj_Vuejs/NOTELoopAndCondition/LoopAndCondition.js
@@ -0,0 +1,17 @@
+var vueInstance = new Vue({
+    el: '.app', 
+    data: {
+        users: [
+            {name: 'Thang', age: 20, isActive: false},
+            {name: 'Hai',   age: 18, isActive: true}, 
+            {name: 'Anh',   age: 16, isActive: false},
+            {name: 'Quang', age: 15, isActive: true}
+        ]
+    }, 
+    methods: {
+       
+    }, 
+    computed: {
+        
+    }
+});
