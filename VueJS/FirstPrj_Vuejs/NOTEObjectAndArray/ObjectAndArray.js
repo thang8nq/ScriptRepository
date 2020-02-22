c20a8e26c0c1753ce02fff91ce96e575b8da5342
thang8nq <thang8nq@gmail.com>
2020-02-22 12:09:38 +0700

    [VueJs] Array and Object

---
+++ VueJS/FirstPrj_Vuejs/NOTEObjectAndArray/ObjectAndArray.js
@@ -0,0 +1,16 @@
+var vueInstance = new Vue({
+    el: '#app', 
+    data: {
+        arrNumber: [1,2,3,4,5], 
+        objectScore: [
+            "math": 8,
+
+        ]
+    }, 
+    methods: {
+       
+    }, 
+    computed: {
+        
+    }
+});
