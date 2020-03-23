9678ea3e7d5ae5230da14571fda6447c349f2362
thang8nq <thang8nq@gmail.com>
2020-03-24 00:26:10 +0700

    [VueJs] Component

--- VueJS/CLI/WebpackSimple/src/App.vue
+++ VueJS/CLI/WebpackSimple/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <!-- <CompHeader></CompHeader> -->
+    <!-- <CompHeader /> -->
+    <comp-header />
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
@@ -16,16 +19,23 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <CompFooter />
   </div>
 </template>
 
 <script>
+import CompHeader from './components/CompHeader';
+import CompFooter from './components/CompFooter';
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  }, 
+  components: {
+    CompHeader, 
+    CompFooter
   }
 }
 </script>
---
+++ VueJS/CLI/WebpackSimple/src/components/CompFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer><h1>{{ text }}</h1></footer>
+</template>
+
+<script>
+export default {
+    name: 'comp-footer', 
+    data() {
+        return {
+            text: 'Comp Footer'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
