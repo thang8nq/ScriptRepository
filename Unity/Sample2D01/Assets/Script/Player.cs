9ccb142e2c4dc1f543597a13f52f503e476a5a62
Nguyen Quang Thang <thang.nguyenquang@gameloft.com>
2020-07-20 17:45:26 +0700

    [UNITY] player jump

--- Unity/Sample2D01/Assets/Script/Player.cs
+++ Unity/Sample2D01/Assets/Script/Player.cs
@@ -8,18 +8,38 @@ public class Player : MonoBehaviour {
     public float speed = 50f, maxspeed = 3, jumpPow = 220f;
 
     // Component to change velocity, mass,... 
-    public Rigidbody2D r2; 
+    public Rigidbody2D r2;
 
+
+    // Check status of player on ground or not 
+    public bool grounded = true;
+    public Animator anim; 
 
 	// Use this for initialization, call only once time
 	void Start () {
         // Get component at 1st launch 
-        r2 = gameObject.GetComponent<Rigidbody2D>(); 
+        r2 = gameObject.GetComponent<Rigidbody2D>();
+
+        // Get animator at 1st launch 
+        anim = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // Link Grounded in Animator (from sence) to grounded in script -> update real time 
+        anim.SetBool("Grounded", grounded);
+        anim.SetFloat("Speed", Mathf.Abs(r2.velocity.x)); 
+        
+        // Jump when press Space & player on ground
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                grounded = false; // update image of player when jump (condition in state jump)
+                r2.AddForce(Vector2.up * jumpPow);
+            }
+        }
+
 	}
 
     /// <summary>
@@ -37,8 +57,8 @@ public class Player : MonoBehaviour {
 
         // Limit the player's speed 
         if (r2.velocity.x > maxspeed)
-            r2.velocity = new Vector2(maxspeed, r2.position.y);
-        if (r2.velocity.x < -maxspeed)
+            r2.velocity = new Vector2(maxspeed, r2.position.y); 
+        if(r2.velocity.x < -maxspeed)
             r2.velocity = new Vector2(-maxspeed, r2.position.y);
     }
 }
