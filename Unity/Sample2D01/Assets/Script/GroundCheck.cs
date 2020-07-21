23635c1ee26f40d1b16cc216f127f141e703813d
Nguyen Quang Thang <thang.nguyenquang@gameloft.com>
2020-07-21 09:24:38 +0700

    [UNITY] add script to handle grounded state

---
+++ Unity/Sample2D01/Assets/Script/GroundCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+    public Player player; 
+
+	// Use this for initialization
+	void Start () {
+        player = gameObject.GetComponentInParent<Player>();
+	}
+	
+	/// <summary>
+    /// Checking collision between Box2D trigger with Box2D of wall to detect player on ground or not 
+    /// </summary>
+	void FixedUpdate () {
+		
+	}
+
+    // Start impacting
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.grounded = true; 
+    }
+
+    // Continue impacting (on ground)
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        player.grounded = true;
+    }
+
+    // End impacting
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player.grounded = false;
+    }
+}
