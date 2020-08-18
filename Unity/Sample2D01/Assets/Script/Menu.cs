d9011571265451e36aa2693cb0a707444e5052c3
Nguyen Quang Thang <thang.nguyenquang@gameloft.com>
2020-08-18 09:59:15 +0700

    [UNITY] handle menu, add missing asset from prev commit

---
+++ Unity/Sample2D01/Assets/Script/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    private bool pause = false;
+    public GameObject pauseUI; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseUI = GameObject.FindGameObjectWithTag("PauseUI");
+        pauseUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause = !pause;
+            pauseUI.SetActive(pause);
+        }
+    }
+}
---
+++ Unity/Sample2D01/Assets/Script/MovingPlat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlat : MonoBehaviour
+{
+    public float speed = 0.08f;
+    public float changeDirection = -1;
+    Vector3 Move; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Move = transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move.x  += speed;
+        transform.position = Move; 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Ground"))
+        {
+            speed *= changeDirection;
+        }
+    }
+}
