cbbc02fe836c45382213967d7e33171e673ef266
Nguyen Quang Thang <thang.nguyenquang@gameloft.com>
2020-08-23 00:07:09 +0700

    [UNITY] finish set up for player HP

--- Unity/Sample2D01/Assets/Script/HpUI.cs
+++ Unity/Sample2D01/Assets/Script/HpUI.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HpUI : MonoBehaviour
 {
+    public Sprite[] HpSprite;
+
+    //We will update HP UI base on player's HP (currentHP)
+    public Player player;
+
+    // this is var's name of image in unity 
+    public Image heart; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // currentHP = 5 -> image 5 (6th) 
+        // currentHP = 0 -> image 0 (1st)
+        Debug.Log(player.currentHP);
+        heart.sprite = HpSprite[player.currentHP];
     }
 }
