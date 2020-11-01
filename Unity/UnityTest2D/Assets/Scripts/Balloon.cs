e819c2b2646765f3c7a0850ddf8dc6994aefde88
thang8nq <thang8nq@gmail.com>
2020-11-01 18:48:39 +0700

    moving balloon (upward) -> using Translate with Vector3.up

--- Unity/UnityTest2D/Assets/Scripts/Balloon.cs
+++ Unity/UnityTest2D/Assets/Scripts/Balloon.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Balloon : MonoBehaviour
 {
+    public float maxSpeed = 3.0f; 
+    public float minSpeed = 1.0f; 
+    public float speed = 2.0f; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //random speed 
+        //speed = Random.Range(minSpeed, maxSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
     }
 }
