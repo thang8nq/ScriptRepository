1d88f0f30b14d47e8d4b02b9295142f9256968ae
thang8nq <thang8nq@gmail.com>
2020-11-01 23:15:28 +0700

    spawn continuous balloon with delay timer (cooldown)

--- Unity/UnityTest2D/Assets/Scripts/GameMaster.cs
+++ Unity/UnityTest2D/Assets/Scripts/GameMaster.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class GameMaster : MonoBehaviour
 {
     public GameObject balloon;
+    public float spawnInterval = 0.5f; 
+    public float spawnTimeTotal;  
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 randPos = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
-
-        // Instantiate at position (0, 0, 0) and zero rotation
-        Instantiate(balloon, randPos, Quaternion.identity);
+        
     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spawnTimeTotal += Time.deltaTime;
+        if(spawnTimeTotal > spawnInterval)
+        {
+            Debug.Log("spawn");
+            SpawnBalloon();
+            spawnTimeTotal = 0;
+        }        
+        else
+        {
+            Debug.Log("delay");
+        }
+    }
+
+    void SpawnBalloon()
+    {
+        Vector2 randPos = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
+
+        // Instantiate at position (0, 0, 0) and zero rotation
+        Instantiate(balloon, randPos, Quaternion.identity);
     }
 }
