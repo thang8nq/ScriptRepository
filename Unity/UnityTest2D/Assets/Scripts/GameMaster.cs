47fbc4450a970e30c10ee61cd11aa9763c3995c9
thang8nq <thang8nq@gmail.com>
2020-11-01 18:14:59 +0700

    *spawn a balloon with random position on screen - add layer for balloon if it not be shown - using Instantiate() to spawn object in runtime - using Quaternion.identity if don't want to rotate object

---
+++ Unity/UnityTest2D/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public GameObject balloon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 randPos = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
+
+        // Instantiate at position (0, 0, 0) and zero rotation
+        Instantiate(balloon, randPos, Quaternion.identity);
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
